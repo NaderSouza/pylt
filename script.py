ddf7178c276100c8d2db1b5ebc7c2620bfc2f2b5
NaderSouza <naderhadadsouza@gmail.com>
2025-10-07 16:44:38 -0300

    add py

--- script.py
+++ script.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
+"""
+SCRIPT test for study purposes - 
+"""
 
-print("Mashallah mesmo!")
+# Metadata
+__version__ = "0.1.0"
+__author__ = "nadin"
+__license__ = "MIT"
 
+
+# Imports
+import os
+
+# Language detection and greeting message
+current_language = os.getenv("LANG" "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt-BR":
+    msg = "Olá, Mundo!"
+elif current_language == "fr-FR":
+    msg = "Bonjour, le monde!"
+print(msg)
+
+# Main function execution
+# print("Mashallah mesmo!")
